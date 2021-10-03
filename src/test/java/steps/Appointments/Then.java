344de40beba650902ce96ba5f99843331885c640
rahul.srivastava <rahul.srivastava@g42.ai>
2021-10-03 16:52:27 +0400

    updated assertions

--- src/test/java/steps/Appointments/Then.java
+++ src/test/java/steps/Appointments/Then.java
@@ -9,17 +9,20 @@ public abstract class Then<T extends steps.Country.Then<T>> extends When<T> {
     public static void ThenAppointmentShouldBeCreatedSuccessfully() {
         Assertions.assertEquals(201, responseObject.getStatusCode() );
         Assertions.assertNotNull(responseObject, "The Response Object is null");
+        Assertions.assertNotNull(responseObject.getBody(), "The Response body is null" );
 
     }
 
     public static void ThenItShouldThrowBadRequestStatusCode() {
         Assertions.assertEquals(400, responseObject.getStatusCode());
         Assertions.assertNotNull(responseObject, "The Response Object is null");
+        Assertions.assertNotNull(responseObject.getBody(), "The Response body is null" );
     }
 
     public static void ThenItShouldThrowErrorStatusCode() {
         Assertions.assertEquals(500, responseObject.getStatusCode());
         Assertions.assertNotNull(responseObject, "The Response Object is null");
+        Assertions.assertNotNull(responseObject.getBody(), "The Response body is null" );
     }
 
     public static void ThenAppointmentShouldBeFetchedSuccessfully() {
@@ -32,10 +35,12 @@ public abstract class Then<T extends steps.Country.Then<T>> extends When<T> {
     public static void ThenItShouldThrowNotFoundErrorCode() {
         Assertions.assertEquals(404, responseObject.getStatusCode());
         Assertions.assertNotNull(responseObject, "The Response Object is null");
+        Assertions.assertNotNull(responseObject.getBody(), "The Response body is null" );
     }
 
     public static void ThenAppointmentShouldBeUpdatedSuccessfully(AppointmentStatusEnum statusType) {
         Assertions.assertEquals(200, responseObject.getStatusCode());
         Assertions.assertEquals(statusType.toString(), responseObject.getBody().path("[0].status"));
+        Assertions.assertNotNull(responseObject.getBody(), "The Response body is null" );
     }
 }
